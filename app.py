4fb5970dbcba6b80ce62d1a8d0ee04389c62323c
Przemek Żbikowski <you@example.com>
2020-03-14 22:09:12 +0100

    remove TODO

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# TODO: connect to a local postgresql database
 
 #----------------------------------------------------------------------------#
 # Models.
