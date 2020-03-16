ee022581a5d146c9d241776ff4e4475adc25eb80
Przemek Żbikowski <you@example.com>
2020-03-16 19:56:04 +0100

    add missing fields for venue and run migration

--- app.py
+++ app.py
@@ -33,12 +33,16 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genres = db.Column(db.ARRAY(db.String))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    website = db.Column(db.String)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    currently_seeking_talent = db.Column(db.Boolean)
+    currently_seeking_talent_description = db.Column(db.String)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
